6014c9907c92539e1a57b4e6c08ca7b0a2aa60a8
songzifan <15097835580@163.com>
2019-09-02 19:43:41 +0800

    init repo

---
+++ src/main/java/life/majiang/community/controller/HelloController.java
@@ -0,0 +1,18 @@
+package life.majiang.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by Kiss on 2019/9/2 0002.
+ */
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
